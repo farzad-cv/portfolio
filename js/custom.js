a1bbc54d5e9b50bbf7a17610ebe2789087c52b6b
Evelyn Pei <pei.evelyn.s@gmail.com>
2020-10-19 20:56:30 -0700

    Fixed navbar scroll to fixed

--- js/custom.js
+++ js/custom.js
@@ -49,17 +49,3 @@ $(document).ready(function() {
     });
 
 });
-
-wow = new WOW({
-    animateClass: 'animated',
-    offset: 100
-});
-wow.init();
-document.getElementById('').onclick = function() {
-    var section = document.createElement('section');
-    section.className = 'wow fadeInDown';
-    section.className = 'wow shake';
-    section.className = 'wow zoomIn';
-    section.className = 'wow lightSpeedIn';
-    this.parentNode.insertBefore(section, this);
-};
